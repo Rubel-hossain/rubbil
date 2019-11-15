c0d20f7150ab1cb56541babd882872a71dd38e63
Rubel-hossain <rubel8525@yahoo.com>
2019-11-15 19:25:21 +0600

    projects countdown added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import "./assets/css/style.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HeroArea from "./components/HeroArea";
-import Services from "./components/Services"
-
+import Services from "./components/Services";
+import Technology from "./components/Technology";
+import ProjectsCountdown from "./components/ProjectsCountdown";
 
 function App() {
   return (
     <div className="App">
         <HeroArea/>
         <Services/>
+        <Technology/>
+        <ProjectsCountdown/>
     </div>
   );
 }
